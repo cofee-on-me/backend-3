65d45e2d79ff7ea477c964e7423c0510c7ea61de
Karol Wnuk <k.wnuk@op.pl>
2016-09-30 12:32:15 +0100

    Handling 404 error

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # backend-3
 Backend 3 build using Flask
+
+## Running
+
+`$ python run.py`
+
+## Endpoints
+
+`/part/{id}`
+
+Fetches a part. The available id range is from 1-6.
--- backend/backend.py
+++ backend/backend.py
@@ -4,9 +4,6 @@ from . import app
 
 @app.route("/part/<part_id>")
 def list_parts(part_id):
-    if part_id == "666":
-        abort(404)
-
     parts = {
         "1": {"id": 1, "name": "jQuery is the king!"},
         "2": {"id": 2, "name": "jQuery is the lord!"},
@@ -15,5 +12,8 @@ def list_parts(part_id):
         "5": {"id": 5, "name": "Please use jquery!"},
         "6": {"id": 6, "name": "Jquery is the lord!"},
     }
+
+    if part_id not in parts.keys():
+        abort(404)
 
     return jsonify(parts[part_id])
